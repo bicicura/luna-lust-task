56fda2e04c61a4c39dfba5c6f5571f0c16e5d922
Franco La Mattina <flamattina@sugit.com.ar>
2024-04-16 23:29:07 -0300

    backend: extracting logic to different modules

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,15 @@
 import express from "express";
 import cors from "cors";
+import { storage } from "./fileStorage.js";
+import { combineAudios } from "./audioProcessing.js";
+import { dirname } from "path";
 import multer from "multer";
 import fs from "fs";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-import { exec } from "child_process";
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
-
-// Multer config for storing audio files in fs
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // storing the tmp audio files in 'audios' dir!
-    cb(null, join(dirname(fileURLToPath(import.meta.url)), "audios"));
-  },
-  filename: function (req, file, cb) {
-    // random generated name
-    cb(null, `${file.fieldname}_${Date.now()}.mp3`);
-  },
-});
 
 const upload = multer({ storage: storage });
 
@@ -32,45 +20,29 @@ app.post("/combine-audios", upload.array("audios", 2), (req, res) => {
       .send("Se requieren exactamente dos archivos de audio.");
   }
 
-  const tempFileName1 = req.files[0].path;
-  const tempFileName2 = req.files[1].path;
+  const filePaths = req.files.map((file) => file.path);
+  const destinationPath = dirname(filePaths[0]);
 
-  // Creating .txt file, needed for the method we use with ffmpeg
-  const fileList = `filelist_${Date.now()}.txt`;
-  const fileListPath = join(
-    dirname(fileURLToPath(import.meta.url)),
-    "audios",
-    fileList
-  );
-  const combinedFileName = join(
-    dirname(fileURLToPath(import.meta.url)),
-    "audios",
-    `combined_${Date.now()}.mp3`
-  );
+  combineAudios(
+    filePaths,
+    destinationPath,
+    (error, combinedFileName, fileListPath) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return res.status(500).send("Error al combinar archivos de audio.");
+      }
 
-  fs.writeFileSync(
-    fileListPath,
-    `file '${tempFileName1}'\nfile '${tempFileName2}'`
-  );
-
-  // cmd used for concatenating the files with ffmpeg
-  const command = `ffmpeg -f concat -safe 0 -i "${fileListPath}" -c:a libmp3lame -q:a 2 "${combinedFileName}"`;
-
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return res.status(500).send("Error al combinar archivos de audio.");
+      res.sendFile(combinedFileName, (err) => {
+        if (err) {
+          console.error("Error sending file:", err);
+        }
+        // Clean up temp files and file list
+        filePaths.forEach((filePath) => fs.unlink(filePath, () => {}));
+        fs.unlink(fileListPath, () => {});
+        fs.unlink(combinedFileName, () => {});
+      });
     }
-
-    // Sending combined file to client
-    res.sendFile(combinedFileName, (err) => {
-      // cleaning tmp files and .txt file list Limpiar archivos temporales y el archivo de lista
-      fs.unlink(tempFileName1, () => {});
-      fs.unlink(tempFileName2, () => {});
-      fs.unlink(fileListPath, () => {});
-      fs.unlink(combinedFileName, () => {});
-    });
-  });
+  );
 });
 
 app.listen(port, () => {
